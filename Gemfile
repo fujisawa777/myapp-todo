ee8915d5b2027377ae3c2f7c7bbd03de703980c6
fujisawa777 <66346129+fujisawa777@users.noreply.github.com>
2020-08-07 08:08:02 +0900

    create login

--- Gemfile
+++ Gemfile
@@ -54,6 +54,7 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'bootstrap', '~> 4.1.1'
 gem 'haml-rails'
+gem 'devise'
 
 group :production do
   gem 'pg'
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,6 @@
 class TasksController < ApplicationController
+  before_action :move_to_index, except: [:index, :show]
+
   def index
     @tasks = Task.all
   end
@@ -27,8 +29,16 @@ class TasksController < ApplicationController
     redirect_to tasks_url
   end
 
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
+
   private
     def task_params
       params.require(:task).permit(:title)
     end
+
+
 end
